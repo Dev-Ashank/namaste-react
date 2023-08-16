e5573e15e9f4ad74dee325ed9dd86122618e9b2b
Dev-ashank <kumardivyashankgaurav@gmail.com>
2023-08-16 18:44:46 +0530

    Introduced React

---
+++ App.js
@@ -0,0 +1,31 @@
+
+
+/*
+React.createElement() gives as a react object
+render is what convert this to html.
+*/
+
+/**
+ * create siblings you can pass third element as an array
+ * 
+ * React.createElement("div",{id:"xyz"},[firstSibling,secondSibling])
+ */
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement("div", { id: "child1 " },
+            [
+                React.createElement("h1", {}, "I am from H1 tag"),
+                React.createElement("h2", {}, "I am from H2 tag")
+            ]
+        ),
+        React.createElement("div", { id: "child2 " },
+            [
+                React.createElement("h1", {}, "I am from H1 tag"),
+                React.createElement("h2", {}, "I am from H2 tag")
+            ]
+        )
+    ])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
