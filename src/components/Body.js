af74014f6d7b0a8e4a871be09bb3dd799a4c0e23
Dev-ashank <kumardivyashankgaurav@gmail.com>
2024-01-08 14:16:52 +0530

    Changes made for serverless cors fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -39,7 +39,7 @@ const Body = () => {
 
     const fetchData = async () => {
         try {
-            const data = await fetch(SWIGGY_API_URL);
+            const data = await fetch('/api/proxy');
 
             if (!data.ok) {
                 throw new Error(`Failed to fetch data from SWIGGY API. Status: ${data.status}`);
@@ -138,8 +138,8 @@ const Body = () => {
                 {
 
                     filteredList.map((restraunt) => <Link to={"/restraunt/" + restraunt.info.id}
-                     key={restraunt.info.id}>
-                     {/* {restraunt.data.promoted}?<RestrauntCardPromoted resData={restraunt} />: */}
+                        key={restraunt.info.id}>
+                        {/* {restraunt.data.promoted}?<RestrauntCardPromoted resData={restraunt} />: */}
                         <RestrauntCard resData={restraunt} />
                     </Link>
                     )
